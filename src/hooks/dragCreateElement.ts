eb309896b14dcc3951b67aff491ada308ced5e8d
大动物 <15294745236@163.com>
2024-09-08 11:31:34 +0800

    :bug:修复缩放时距离差错

--- src/hooks/dragCreateElement.ts
+++ src/hooks/dragCreateElement.ts
@@ -6,20 +6,20 @@ import { useElementStyleStore, useLayerThumbnail, usePageStore } from "@/store";
 import { parseCss } from "@/utils/parseCss";
 //距离纠正
 let distanceCorrectionX = 0, distanceCorrectionY = 0;
-let finalX = 0, finalY = 0;
-let pageStore: any
+let finalX:string|number = 0, finalY :string|number = 0;
+
 export default function dragCreateElement(taraget: HTMLDivElement) {
     const layerThumbnail = useLayerThumbnail();
     const elementStyleStore = useElementStyleStore();
+    const pageStore = usePageStore();
     taraget.ondragover = (e: DragEvent) => { e.preventDefault(); }
     taraget.ondrop = (e: DragEvent) => {
         distanceCorrectionX = distanceCorrectionX ? distanceCorrectionX : e.screenX - e.layerX;
         distanceCorrectionY = distanceCorrectionY ? distanceCorrectionY : e.screenY - e.layerY;
-        finalY = e.screenY - distanceCorrectionY + Number(taraget.parentElement!.scrollTop.toFixed(0))
-        finalX = e.screenX - distanceCorrectionX + Number(taraget.parentElement!.scrollLeft.toFixed(0))
+        finalY = e.screenY - distanceCorrectionY + Number(taraget.parentElement!.scrollTop.toFixed(0))-(+getComputedStyle(taraget).marginTop.replace('px', ''));
+        finalX = e.screenX - distanceCorrectionX + Number(taraget.parentElement!.scrollLeft.toFixed(0))-(+getComputedStyle(taraget).marginLeft.replace('px', ''));       
         let directive = e.dataTransfer?.getData('directive').split(':') || [];
         if (directive[0] === 'create') {
-            pageStore = pageStore || usePageStore()
             let element = document.createElement(directive[1]);
             taraget.appendChild(element);
             element.style.cssText = elementStyleStore.getCommonElementStyle(directive[1]) || '';
@@ -31,15 +31,12 @@ export default function dragCreateElement(taraget: HTMLDivElement) {
             layerThumbnail.resetLayerThumbnail(pageStore.curIndex)
             let height = parseCss(element.style.cssText, ['heigth'])['height'].replace('px', '')
             let width = parseCss(element.style.cssText, ['width'])['width'].replace('px', '')
-            
             pageStore.getCurrentPage()?.pageElements.push({
                 id: Date.now(),
                 el: element,
                 parent: taraget,
                 style: element.style.cssText,
                 children: [],
-                resizeHeight: (Number(height) * pageStore.getCurrentPage().zoom).toFixed(0),
-                resizeWidth: (Number(width) * pageStore.getCurrentPage().zoom).toFixed(0),
                 height: width,
                 width: height,
             })
--- src/utils/drag.ts
+++ src/utils/drag.ts
@@ -2,10 +2,6 @@ export default function drag(element: HTMLElement) {
  element.ondragstart=(e:DragEvent)=>{
     (e.dataTransfer as DataTransfer).dropEffect="move";
     (e.dataTransfer as DataTransfer).effectAllowed="move";
-
-    console.log(e.dataTransfer);
-    
-    console.log(console.log("start",e));
  }  
  element.ondragenter=(e:DragEvent)=>{
     console.log(console.log("enter",e));
